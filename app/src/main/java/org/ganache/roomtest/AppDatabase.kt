0ba138b67d978e018a9adbac87694c1fc77658b6
이성우 <hisungwoo007@naver.com>
2021-03-21 23:08:15 +0900

    Initial commit

---
+++ app/src/main/java/org/ganache/roomtest/AppDatabase.kt
@@ -0,0 +1,9 @@
+package org.ganache.roomtest
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Todo::class] , version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun todoDao() : TodoDao
+}
